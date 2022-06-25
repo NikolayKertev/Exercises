4717eb45c0968afd8171367e5d0709fda184c1c9
NikolayKertev <nikolaikertev@abv.bg>
2022-06-25 15:55:40 +0300

    Homework

---
+++ Stack and Queues/Stacks and Queues/Fast Food/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fast_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> ordersQueue = new Queue<int>(orders);
+
+            Console.WriteLine(ordersQueue.Max());
+
+            bool hasFood = true;
+
+            while (hasFood && ordersQueue.Any())
+            {
+                int currentOrder = ordersQueue.Peek();
+
+                if (foodQuantity - currentOrder >= 0)
+                {
+                    foodQuantity -= currentOrder;
+                    ordersQueue.Dequeue();
+                }
+                else
+                {
+                    hasFood = false;
+                }
+            }
+
+            if (ordersQueue.Any())
+            {
+                Console.WriteLine($"Orders left: {String.Join(" ", ordersQueue)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+
+
+        }
+    }
+}
---
+++ Stack and Queues/Stacks and Queues/Max nad Min Element/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Max_nad_Min_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int action = input[0];
+
+                switch (action)
+                {
+                    case 1:
+                        int number = input[1];
+                        stack.Push(number);
+                        break;
+                    case 2:
+                        if (stack.Any())
+                        {
+                            stack.Pop();
+                        }
+
+                        break;
+                    case 3:
+                        if (stack.Any())
+                        {
+                            Console.WriteLine(stack.Max());
+                        }
+                        break;
+                    case 4:
+                        if (stack.Any())
+                        {
+                            Console.WriteLine(stack.Min());
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", stack));
+        }
+    }
+}
